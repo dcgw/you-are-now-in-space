dcgw/delta-station

Statically allocate two Points, to reduce garbage collection. (FlashPunk provides two statically allocated points but it's not really safe to use them since FlashPunk sometimes expects them to retain their values after calling user code ;_;).

src/net/noiseinstitute/game/Static.as
@@ -1,0 +1,10 @@
+package net.noiseinstitute.game {
+    import flash.geom.Point;
+
+    /** Various statically allocated resources, to reduce garbage collection.
+     * Don't expect any of these to retain their value across function calls! */
+    public class Static {
+        public static const point:Point = new Point();
+        public static const point2:Point = new Point();
+    }
+}

dcgw/you-are-now-in-space

src/net/noiseinstitute/basecode/Static.as
@@ -1,0 +1,10 @@
+package net.noiseinstitute.basecode {
+    import flash.geom.Point;
+
+    /** Various statically allocated resources, to reduce garbage collection.
+     * Don't expect any of these to retain their value across function calls! */
+    public class Static {
+        public static const point:Point = new Point();
+        public static const point2:Point = new Point();
+    }
+}