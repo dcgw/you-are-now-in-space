dcgw/delta-station

Copy in utility function from 'week' that wraps numbers within a certain range.

src/net/noiseinstitute/game/Range.as
@@ -1,0 +1,16 @@
+package net.noiseinstitute.game {
+    public class Range {
+        public static function wrap(n:Number, min:Number, max:Number):Number {
+            if (n < min || n > max) {
+                n -= min;
+                max -= min;
+                if (n < 0) {
+                    n += max * Math.ceil(-n/max);
+                }
+                return min + n%max;
+            } else {
+                return n;
+            }
+        }
+    }
+}

dcgw/you-are-now-in-space

src/net/noiseinstitute/basecode/Range.as
@@ -1,0 +1,16 @@
+package net.noiseinstitute.basecode {
+    public class Range {
+        public static function wrap(n:Number, min:Number, max:Number):Number {
+            if (n < min || n > max) {
+                n -= min;
+                max -= min;
+                if (n < 0) {
+                    n += max * Math.ceil(-n/max);
+                }
+                return min + n%max;
+            } else {
+                return n;
+            }
+        }
+    }
+}