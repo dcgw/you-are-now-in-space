b23fb67be710d585211c06202db8dbc042be820c
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-02-23 04:43:15 +0000

    refactor: remove useless 'Game' class

--- index.ts
+++ index.ts
@@ -4,21 +4,19 @@ import Player from "./actors/player/player";
 import resources from "./resources";
 import LevelOne from "./scenes/level-one/level-one";
 
-class Game extends Engine {
-    constructor() {
-        super({width: 384, height: 288, displayMode: DisplayMode.FullScreen});
-    }
-}
-
 domready(() => {
-    const game = new Game();
-    const levelOne = new LevelOne(game);
+    const engine = new Engine({
+        width: 384,
+        height: 288,
+        displayMode: DisplayMode.FullScreen
+    });
+    const levelOne = new LevelOne(engine);
     const player = new Player();
     player.addDrawing(resources.Sword);
 
     levelOne.add(player);
 
-    game.add("levelOne", levelOne);
+    engine.add("levelOne", levelOne);
 
 
     const loader = new Loader();
@@ -26,7 +24,7 @@ domready(() => {
         loader.addResource(resources[key]);
     }
 
-    game.start(loader)
-        .then(() => game.goToScene("levelOne"),
+    engine.start(loader)
+        .then(() => engine.goToScene("levelOne"),
             reason => console.error("", reason));
 });
