82915701ce3aa1af08ffac9cac0fed0fd9a48524
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-02-23 06:08:19 +0000

    feat(Title): add ogg format title music

--- resources.ts
+++ resources.ts
@@ -1,12 +1,13 @@
 import {Sound, Texture} from "excalibur";
-import titleMusic from "./scenes/title/Title.mp3";
+import titleMusicMp3 from "./scenes/title/Title.mp3";
+import titleMusicOgg from "./scenes/title/Title.ogg";
 import title from "./scenes/title/Title.png";
 import titleImage from "./scenes/title/TitleImage.png";
 
 const resources = {
     title: new Texture(title),
     titleImage: new Texture(titleImage),
-    titleMusic: new Sound(titleMusic)
+    titleMusic: new Sound(titleMusicOgg, titleMusicMp3)
 };
 
 export default resources;
