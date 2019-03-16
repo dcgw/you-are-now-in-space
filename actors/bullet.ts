658dbef6abae3e9a935830bc091312cfe1415c36
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-03-16 21:17:41 +0000

    refactor(bullet): simplify bullet actor

--- actors/bullet.ts
+++ actors/bullet.ts
@@ -1,18 +1,14 @@
-import {Actor, Engine} from "excalibur";
+import {Actor, Vector} from "excalibur";
 import resources from "../resources";
 
 const width = 5;
 const height = 10;
 
-const speed = 10 * 60 / 1000;
+const speed = 10 * 60;
 
 export default class Bullet extends Actor {
     constructor(x: number, y: number) {
-        super({x, y, width, height});
+        super({x, y, width, height, vel: new Vector(0, speed)});
         this.addDrawing(resources.bullet);
-    }
-
-    public update(engine: Engine, delta: number): void {
-        this.y += speed * delta;
     }
 }
