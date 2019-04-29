c6e6d50cdb8abd3b03cf3477f55a832531e4d363
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-04-29 05:49:44 +0100

    feat(level-1): add alien formation to level 1

--- scenes/level-1/index.ts
+++ scenes/level-1/index.ts
@@ -1,4 +1,5 @@
 import {Color, Scene} from "excalibur";
+import Formation from "../../aliens/formation";
 import Game from "../../game";
 import Border from "../../hud/border";
 import Score from "../../hud/score";
@@ -8,9 +9,18 @@ export default class Level1 extends Scene {
     constructor(private readonly game: Game) {
         super(game.engine);
 
-        this.add(new Player(game));
+        const player = new Player(game);
+        this.add(player);
+
         this.add(new Score(game));
         this.add(new Border(game));
+
+        const formation = new Formation(game, player);
+        this.add(formation);
+
+        for (const alien of formation.getAliens()) {
+            this.add(alien);
+        }
     }
 
     public onActivate(): void {
