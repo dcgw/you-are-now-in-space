859392ecd2e123bfecc51ccfacc7b240ae908b59
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-09-22 17:11:15 +0100

    feat(level-1): count down score timer, advance level when it reaches zero

--- scenes/level-1/index.ts
+++ scenes/level-1/index.ts
@@ -1,4 +1,4 @@
-import {Color, Scene} from "excalibur";
+import {Color, Engine, Scene} from "excalibur";
 import Formation from "../../aliens/formation";
 import Game from "../../game";
 import Border from "../../hud/border";
@@ -6,6 +6,8 @@ import Score from "../../hud/score";
 import Player from "../../player/player";
 
 export default class Level1 extends Scene {
+    private scoreTime = 0;
+
     constructor(private readonly game: Game) {
         super(game.engine);
 
@@ -25,5 +27,21 @@ export default class Level1 extends Scene {
 
     public onActivate(): void {
         this.game.engine.backgroundColor = Color.fromHex("000000");
+        this.scoreTime = 1200 * 1000 / 60;
+    }
+
+    public update(engine: Engine, delta: number): void {
+        super.update(engine, delta);
+
+        this.scoreTime -= delta;
+
+        if (this.scoreTime <= 0) {
+            this.finish();
+        }
+    }
+
+    private finish(scene = "get-ready"): void {
+        ++this.game.stage;
+        this.game.engine.goToScene(scene);
     }
 }
