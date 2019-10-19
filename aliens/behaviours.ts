6106a3d6d7722f65c33d28e7a5fbc57c402035bb
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-10-19 15:59:29 +0100

    feat(behaviours): implement Alien Level 3 behaviour

--- aliens/behaviours.ts
+++ aliens/behaviours.ts
@@ -6,7 +6,7 @@ export interface Behaviour {
     update(delta: number): void;
 }
 
-const speed = 60 / 1000;
+const brokenFormationSpeed = 60 / 1000;
 
 export class BrokenFormationBehaviour implements Behaviour {
     private layer = Math.random() * 3 + 2;
@@ -15,14 +15,14 @@ export class BrokenFormationBehaviour implements Behaviour {
     }
 
     public update(delta: number): void {
-        this.alien.pos.y -= speed * this.layer * delta;
+        this.alien.pos.y -= brokenFormationSpeed * this.layer * delta;
 
         if (this.game.engine.input.keyboard.isHeld(Input.Keys.Left)) {
-            this.alien.pos.x -= speed * this.layer * delta;
+            this.alien.pos.x -= brokenFormationSpeed * this.layer * delta;
         }
 
         if (this.game.engine.input.keyboard.isHeld(Input.Keys.Right)) {
-            this.alien.pos.x += speed * this.layer * delta;
+            this.alien.pos.x += brokenFormationSpeed * this.layer * delta;
         }
 
         if (this.alien.pos.y <= -height) {
@@ -36,5 +36,25 @@ export class BrokenFormationBehaviour implements Behaviour {
         if (this.alien.pos.x >= this.game.width) {
             this.alien.pos.x -= this.game.width;
         }
+    }
+}
+
+const level3Speed = 0.2 * 60 / 1000;
+
+export class Level3Behaviour implements Behaviour {
+    private pos = Math.random() * Math.PI * 2;
+
+    private pathLeft = Math.random() * this.game.playWidth / 2 + this.game.playWidth / 4;
+    private pathTop = Math.random() * this.game.playHeight / 2 + this.game.playHeight / 4;
+    private pathWidth = Math.random() * this.pathLeft - this.game.playWidth / 4;
+    private pathHeight = Math.random() * this.pathTop - this.game.playHeight / 4;
+
+    constructor(private readonly game: Game, private readonly alien: Alien) {
+    }
+
+    public update(delta: number): void {
+        this.pos += level3Speed * this.game.stage;
+        this.alien.pos.x = this.game.playLeft + this.pathLeft + this.pathWidth * Math.cos(this.pos);
+        this.alien.pos.y = this.game.playTop + this.pathTop + this.pathHeight * math.sin(pos);
     }
 }
